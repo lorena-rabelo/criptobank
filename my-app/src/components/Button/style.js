fb3f5fe5b371872f2606ec5cc3ef0d3745353343
Lorena Rabelo <ola.lorenarabelo@gmail.com>
2022-09-07 22:59:36 -0300

    Adding button component

---
+++ my-app/src/components/Button/style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const BaseButton = styled.button`
+  width: 300px;
+  height: 48px;
+  background-color: #cdfc51;
+  border-radius: 24px;
+  color: #201f22;
+  padding: 8px 16px;
+  font-size: 16px;
+`;
