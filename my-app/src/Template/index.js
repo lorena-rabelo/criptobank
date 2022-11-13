e93f5f979bb59802b8a239c30146b91ec5e1ac08
Lorena Rabelo <ola.lorenarabelo@gmail.com>
2022-11-13 18:15:26 -0300

    adding template component

--- my-app/src/Template/index.js
+++ my-app/src/Template/index.js
@@ -1,17 +1,18 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
-import cripto from '../assets/img/logo.svg'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import cripto from "../assets/img/logo.svg";
 
-import * as S from './style'
+import * as S from "./style";
 
 const Template = ({ children }) => {
+  return (
+    <S.Wrapper>
+      <a href="/">
+        <S.Logo src={cripto} />
+      </a>
+      {children}
+    </S.Wrapper>
+  );
+};
 
-    return (
-        <S.Wrapper>
-            <S.Logo src={cripto}/>
-            {children}
-        </S.Wrapper>
-    )
-}
-
-export default Template
+export default Template;
