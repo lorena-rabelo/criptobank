6ebeb09a81a3cac351b889f1fed347aeaccc93fa
Lorena Rabelo <ola.lorenarabelo@gmail.com>
2022-11-22 17:57:33 -0300

    updating dashboard

--- my-app/src/pages/Dashboard/index.js
+++ my-app/src/pages/Dashboard/index.js
@@ -1,57 +1,109 @@
 import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import Input from "../../components/Input";
+import Button from "../../components/Button";
 import trash from "../../assets/img/icon-trash.svg";
 import api from "../../service/api";
+import Loading from "../../components/Loading";
 
 import * as S from "./style";
 
 const Dashboard = () => {
-  const [name, setName] = useState('')
-  useEffect(() => {
-    console.log(localStorage.getItem("token"));
-  }, []);
+  const navigate = useNavigate();
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [name, setName] = useState("");
+  const [accountsList, setAccountsList] = useState([]);
+  const [accountInfo, setAccountInfo] = useState([]);
+  const [showTransferModal, setShowTransferModal] = useState(false);
+  const [showCreateAccountModal, setCreateAccountModal] = useState(false);
 
   useEffect(() => {
     api
       .get("/api/customer/user", {
         headers: {
-          Authorization: `Bearer ${localStorage.getItem("token")}`,
+          Authorization: `Bearer ${sessionStorage.getItem("token")}`,
         },
       })
       .then((response) => {
-        console.log(response.data);
-        setName(response.data.name)
+        setName(response.data.name);
+      })
+      .catch((err) => {
+        console.error("ops! ocorreu um erro : " + err);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, []);
+
+  useEffect(() => {
+    api
+      .get("api/account/accountList", {
+        headers: {
+          Authorization: `Bearer ${sessionStorage.getItem("token")}`,
+        },
+      })
+      .then((response) => {
+        setAccountsList(response.data);
       })
       .catch((err) => {
         console.error("ops! ocorreu um erro : " + err);
       });
   }, []);
 
-
   useEffect(() => {
     api
-      .get("/api/customer/user", {
+      .get(`api/account/transactions/${accountsList[0]?.account_number}`, {
         headers: {
-          Authorization: `Bearer ${localStorage.getItem("token")}`,
+          Authorization: `Bearer ${sessionStorage.getItem("token")}`,
         },
       })
       .then((response) => {
-        console.log(response.data);
-        setName(response.data.name)
-        localStorage.removeItem('token');
+        console.log("response: transactions info ", response);
+        setAccountInfo(response.data);
       })
       .catch((err) => {
         console.error("ops! ocorreu um erro : " + err);
       });
-  }, []);
+  }, [accountsList]);
 
-  return (
+  // useEffect(() => {
+  //   console.log('accounts[0]: ', accounts[0]);
+  // }, [accounts]);
+
+  const handleLogout = () => {
+    api
+      .delete("/api/customer/delete", {
+        headers: {
+          Authorization: `Bearer ${sessionStorage.getItem("token")}`,
+        },
+      })
+      .then((response) => {
+        console.log("response: transactions info ", response);
+        //!  ver o código de sucesso;
+        navigate("/");
+      })
+      .catch((err) => {
+        console.error("ops! ocorreu um erro : " + err);
+        sessionStorage.removeItem("token");
+        navigate("/");
+      });
+  };
+
+  const handleTransfer = () => {
+    console.log("transferiu");
+  };
+
+  return isLoading ? (
+    <Loading spinner />
+  ) : (
     <S.Wrapper>
       <S.PersonalInfo>
         <S.Avatar />
         <span>{`Olá, ${name}`}</span>
         <S.EditInfo to="/edit">Editar informações</S.EditInfo>
 
-        <S.CustomLink to="/register"> Sair </S.CustomLink>
+        <S.CustomButton onClick={handleLogout}> Sair </S.CustomButton>
       </S.PersonalInfo>
       <S.Dashboard>
         <div>
@@ -64,43 +116,76 @@ const Dashboard = () => {
             </div>
 
             <div>
-              <S.CreateNewAccountButton onClick={() => console.log("ratanaba")}>
+              <S.CreateNewAccountButton
+                onClick={() => setShowTransferModal(true)}
+              >
                 +
               </S.CreateNewAccountButton>
               <span>Transferência</span>
             </div>
           </S.NewAccountWrapper>
+
           <S.AccountsWrapper>
-            <S.Account>
-              <S.DeleteAccountButton onClick={() => console.log("ratanaba")}>
-                <img src={trash} />
-              </S.DeleteAccountButton>
-              <span>conta 123</span>
-              <S.AccountValue>R$ 3434,00</S.AccountValue>
-            </S.Account>
-
-            <S.Account>
-              <S.DeleteAccountButton onClick={() => console.log("ratanaba")}>
-                <img src={trash} />
-              </S.DeleteAccountButton>
-              <span>conta 123</span>
-              <S.AccountValue>R$ 3434,00</S.AccountValue>
-            </S.Account>
-
-            <S.Account>
-              <S.DeleteAccountButton onClick={() => console.log("ratanaba")}>
-                <img src={trash} />
-              </S.DeleteAccountButton>
-              <span>conta 123</span>
-              <S.AccountValue>R$ 3434,00</S.AccountValue>
-            </S.Account>
+            {accountsList.map((item) => (
+              <S.Account key={item.id}>
+                <S.DeleteAccountButton
+                  onClick={() => setShowTransferModal(false)}
+                >
+                  <img src={trash} />
+                </S.DeleteAccountButton>
+                <span>{`nº ${item.account_number}`}</span>
+                <S.AccountValue>
+                  {item.balance.toLocaleString("pt-br", {
+                    style: "currency",
+                    currency: "BRL",
+                  })}
+                </S.AccountValue>
+              </S.Account>
+            ))}
           </S.AccountsWrapper>
-          <S.Transactions>
+
+          <S.TransactionsWrapper>
             <span>Transações</span>
             <hr></hr>
-          </S.Transactions>
+            <S.Transaction>
+              <span>tipo</span>
+              <span>{accountInfo[0]?.transaction_type}</span>
+              <span>valor</span>
+              <span>{accountInfo[0]?.value}</span>
+            </S.Transaction>
+          </S.TransactionsWrapper>
         </div>
       </S.Dashboard>
+
+      {showTransferModal && (
+        <S.ModalWrapper>
+          <S.Content role="dialog" aria-modal={showTransferModal}>
+            <S.CloseButton onClick={() => setShowTransferModal(false)}>
+              x
+            </S.CloseButton>
+            <span>Transferência entre contas</span>
+            <form onSubmit={handleTransfer}>
+              <S.ContentForm>
+                <S.CustomSelect name="select">
+                  <option value="" selected>
+                    Escolha uma conta de saída
+                  </option>
+                  {accountsList.map((item) => (
+                    <option value={item.id}>{item.account_number}</option>
+                  ))}
+                </S.CustomSelect>
+                <Input darkColor type="text" placeholder="conta"></Input>
+                <Input
+                  darkColor
+                  type="text"
+                  placeholder="valor a transferir"
+                ></Input>
+              </S.ContentForm>
+              <Button type="submit">Transferir</Button>
+            </form>
+          </S.Content>
+        </S.ModalWrapper>
+      )}
     </S.Wrapper>
   );
 };
--- my-app/src/pages/Dashboard/style.js
+++ my-app/src/pages/Dashboard/style.js
@@ -13,9 +13,14 @@ export const Dashboard = styled.section`
   padding: 40px;
 `;
 
-export const Transactions = styled.div`
-text-align: start;
-font-size: 24px;
+export const TransactionsWrapper = styled.div`
+  text-align: start;
+  font-size: 24px;
+`;
+
+export const Transaction = styled.div`
+  display: flex;
+  justify-content: space-between;
 `;
 
 export const PersonalInfo = styled.aside`
@@ -57,12 +62,12 @@ export const NewAccountWrapper = styled.div`
 export const AccountsWrapper = styled.div`
   margin: 36px 0;
   display: flex;
-  padding: 16px 32px;
+  padding: 16px 0;
 `;
 
 export const Account = styled.div`
-  width: 160px;
-  height: 100px;
+  width: 180px;
+  /* height: 100px; */
   display: flex;
   border-radius: 16px;
   margin-right: 16px;
@@ -70,24 +75,20 @@ export const Account = styled.div`
   border: 1px solid black;
   display: flex;
   flex-direction: column;
-
-  :first-child {
-    text-align: left;
-    margin-bottom: 24px;
-  }
+  text-align: initial;
 `;
 
 export const AccountValue = styled.div`
   font-size: 20px;
   font-weight: 500;
   margin-top: 8px;
+  text-align: initial;
 `;
 
 export const EditInfo = styled(Link)`
   text-decoration: none;
   color: #cdfc51;
   font-size: 18px;
-  /* margin-top: 16px; */
 `;
 
 export const CreateNewAccountButton = styled.button`
@@ -108,9 +109,76 @@ export const DeleteAccountButton = styled.div`
   }
 `;
 
-export const CustomLink = styled(Link)`
-  text-decoration: none;
+export const CustomButton = styled.button`
   color: #cdfc51;
   font-size: 18px;
-  margin-top: 16px;
+  margin-top: 36px;
+  background-color: transparent;
+  border: none;
+  text-align: left;
+  padding: 0;
+`;
+
+export const ModalWrapper = styled.section`
+  display: flex;
+  position: fixed;
+  margin: 0;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: #0000004d;
+  z-index: 2147483601;
+`;
+
+export const Content = styled.div`
+  background-color: white;
+  left: 50%;
+  position: absolute;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  padding: 40px;
+  box-shadow: 0px 2px 4px #00000029;
+  border-radius: 8px;
+  max-height: 80%;
+  overflow: auto;
+  ${(props) => props.customCss ?? ""}
+
+  > span {
+    font-size: 20px;
+  }
+`;
+
+export const CustomSelect = styled.select`
+  background-color: transparent;
+  width: 100%;
+  height: 48px;
+  border: 2px solid #201f22;
+  border-radius: 24px;
+  color: #201f22;
+  padding: 8px 16px;
+  font-size: 16px;
+  &::placeholder {
+    color: #201f22;
+  }
+
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+`;
+
+export const CloseButton = styled.span`
+  position: absolute;
+  right: 20px;
+  top: 20px;
+`;
+
+export const ContentForm = styled.form`
+  margin-top: 24px;
+  > * {
+    margin: 16px 0;
+  }
+  > select {
+    margin: 0;
+  }
 `;
