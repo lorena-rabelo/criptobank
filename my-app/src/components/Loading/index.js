27589db768a5f530b687073ddc960c9570a6890c
Lorena Rabelo <ola.lorenarabelo@gmail.com>
2022-11-22 18:01:02 -0300

    adding component Loading

---
+++ my-app/src/components/Loading/index.js
@@ -0,0 +1,8 @@
+import spinnerSvg from "../../assets/img/spinner.svg";
+import React from "react";
+
+const Loading = () => {
+  return <img src={spinnerSvg} alt="Spinner Loading" id="spinner-loading" />;
+};
+
+export default Loading;
