4ca128677d8aa7601c69e8faa2f1313a6e8af805
Lorena Rabelo <ola.lorenarabelo@gmail.com>
2022-11-22 17:53:36 -0300

    adding EditPersonalInfo

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,16 +5,20 @@ import Template from "./Template";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
+import EditPersonalInfo from "./pages/EditPersonalInfo";
 
 function App() {
   return (
     <BrowserRouter>
       <Template>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="register" element={<Register />} />
-          <Route path="dashboard" element={<Dashboard />} />
-        </Routes>
+
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="register" element={<Register />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="edit" element={<EditPersonalInfo />} />
+          </Routes>
+
       </Template>
     </BrowserRouter>
   );
